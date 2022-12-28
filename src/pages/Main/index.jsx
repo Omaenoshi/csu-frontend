4a86cea8634655988b6bd02fb638eb1e4ca5f55e
Omaenoshi <71465546+Omaenoshi@users.noreply.github.com>
2022-12-29 02:06:21 +0500

    changed css imports

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import HeaderMenu from '@Components/Header/headerMenu';
-import TablePage from '@Components/Main/Main';
-import headerStyles from '../../components/Header/forHeader.css';
-import mainStyles from '../../components/Main/forMain.css';
-import footerStyles from '../../components/Footer/forFooter.css';
+import HeaderMenu from '@Components/Header/HeaderMenu';
+import TablePage from '@Components/TablePage/TablePage';
 import Footer from '@Components/Footer/Footer';
 
 const Main = () => (
     <div className="main-page">
-        <HeaderMenu headerStyles={headerStyles} />
-        <TablePage mainStyles={mainStyles} />
-        <Footer footerStyles={footerStyles} />
+        <HeaderMenu/>
+        <TablePage/>
+        <Footer/>
     </div>
 );
 
